fe0ef6e485dde0de295a4fbf3d29abc2f4e83c84
Turkay Tunc <turkaytunc00@gmail.com>
2020-06-04 13:19:06 +0300

    added singleton pattern

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+
+    private static ObjectPool objectPoolInstance;
+
+
+    private void Awake()
+    {
+        if (objectPoolInstance == null)
+        {
+            objectPoolInstance = this;
+        }
+        else if(objectPoolInstance != this)
+        {
+            Debug.Log("More than one pool instance");
+        }
+
+    }
+
+
+}
