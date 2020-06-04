2514d48ee1c08972109e74b21f85379aca408206
Turkay Tunc <turkaytunc00@gmail.com>
2020-06-04 13:49:40 +0300

    added player

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+
+    private float movementSpeed = 10f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+    }
+}
